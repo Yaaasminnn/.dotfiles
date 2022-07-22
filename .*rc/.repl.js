7d4a60d5f03ddab349ec62c22cc0c20b650f07f9
MonEmperor <abr.da.king2016@gmail.com>
2022-07-22 04:00:58 -0400

    added repl.js and updated pythonrc

---
+++ .*rc/.repl.js
@@ -0,0 +1,19 @@
+// REPL.js file
+// Equivalent of a .*rc file, but for JS
+// loads all variables and methods etc
+
+// all imports here
+const repl = require('repl');
+
+// load all variables here
+const prompt = "[\033[0;33;1mjs\033[0;1m] \033[0;33;1m-> \033[0m"
+
+
+
+const local = repl.start(prompt) // starts the repl
+
+
+local.on('exit', () => { // what to do on exit
+  console.log("exiting...")
+  process.exit()
+})
