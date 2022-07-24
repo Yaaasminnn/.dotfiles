afa80933908b72e398ba923bf54ee176014d1ad0
MonEmperor <46490084+MonEmperor@users.noreply.github.com>
2022-07-24 01:33:28 -0400

    Update setup.sh

---
+++ setup.sh
@@ -0,0 +1,19 @@
+# Setup file
+# moves all the files in the repo and setup the config.
+#
+# Basically just makes all the appropriate directories, 
+# moves all the files and sets the perms,
+# and git clones or curl/wget's any remote files.
+#
+# does not download packages except for the ones needed to perform this script
+#
+# todo:
+#   .*rc files
+#   .config
+#   autostart
+#   calculators -> ~/programming/scripts/calculators
+#   crontab
+#   kwin rules?
+#   autokey macros
+#   scripts -> ~/programming/scripts/
+#   vim plugins install? need fonts
