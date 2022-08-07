caa8fde7bf8e729c509d456d90944bba0c7b7262
MonEmperor <46490084+MonEmperor@users.noreply.github.com>
2022-08-07 00:28:03 -0400

    fixed link to .git-prompt.sh
    i made a typo

--- setup.sh
+++ setup.sh
@@ -50,7 +50,7 @@ rm -r /home/$user/.vim/
 # makes links in ~
 ln -s /home/$user/.dotfiles/.\*rc/.bashrc /home/$user/.bashrc
 ln -s /home/$user/.dotfiles/.\*rc/.gitconfig /home/$user/.gitconfig
-ln -s /home/$user/.dotfiles/.\*rc/git-prompt.sh /home/$user/.git-prompt.sh
+ln -s /home/$user/.dotfiles/.\*rc/.git-prompt.sh /home/$user/.git-prompt.sh
 ln -s /home/$user/.dotfiles/.\*rc/.pythonrc  /home/$user/.pythonrc
 ln -s /home/$user/.dotfiles/.\*rc/.replrc /home/$user/.replrc
 ln -s /home/$user/.dotfiles/vim/.vimrc /home/$user/.vimrc
